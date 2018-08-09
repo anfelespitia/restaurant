980b15dc0e774bf000f129e5edb4392299bfa9ab
Khaled Tayeb <ktayeb86@hotmail.com>
2018-08-09 19:24:01 +0200

    Update Files

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 445 // Change this to your server port
-    return `https://mohammedriaad.github.io/restaurant/:${port}/data/restaurants.json`;
+    const port = 80 // Change this to your server port
+    return `https://mohammedriaad.github.io/restaurant/data/restaurants.json`;
   }
 
   /**
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
