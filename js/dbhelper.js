d29a69da2d4bf7d6f323e58b12fab77b9e550b63
Khaled Tayeb <38245969+Romerous@users.noreply.github.com>
2018-08-09 19:41:52 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 80 // Change this to your server port
-    return `https://romerous.github.io/mws-restaurant-stage-1-google-maps/data/restaurants.json`;
+    return `https://romerous.github.io/restaurant/data/restaurants.json`;
   }
 
   /**
